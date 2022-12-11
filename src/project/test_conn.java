d5c543f82a2efc35b2bc7e170e607327d162df45
Rich627 <103445968+Rich627@users.noreply.github.com>
2022-12-11 20:26:17 +0800

    12/11

--- src/project/test_conn.java
+++ src/project/test_conn.java
@@ -7,11 +7,24 @@ public class test_conn {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		String urlstr = "jdbc:sqlserver://140.136.158.122MA402-PC25:1433;databaseName=project;user=project_account;password=helloworld";
+		String urlstr = "jdbc:sqlserver://140.136.158.12:1433;databaseName=Project;user=project_account;password=helloworld";
+
 			
 		Connection conn = DriverManager.getConnection(urlstr);
 		boolean status = !conn.isClosed();
 		System.out.println("connection status" + status);
 	}
+	
+	public void closeConn() throws SQLException {
+		if (conn != null) {
+			conn.close();
+		}
+	}
+	
+	public void insertDb() throws SQLException{
+		Statement state = conn.createStatement();
+		String sqlstr = "insert into xxx";
+		in count = stae.executeUpdate(sqlstr);
+	}
+	
 }
-
