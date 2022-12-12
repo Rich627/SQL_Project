8871f7332d7b2fe9db90bd047cc0c5a4a61ebb7a
Rich627 <103445968+Rich627@users.noreply.github.com>
2022-12-12 14:46:10 +0800

    connsql failed add feature login

---
+++ src/project/loginin.java
@@ -0,0 +1,26 @@
+package project;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class loginin {
+private Connection conn;
+	
+	public void createConn() throws ClassNotFoundException, SQLException{
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		
+		String urlstr = "jdbc:sqlserver://140.136.158.12:1433;databaseName=Project;user=project_account;password=helloworld";
+		conn = DriverManager.getConnection(urlstr);
+		boolean status = !conn.isClosed();
+		System.out.println("connection status" + status);
+	}
+	
+	public void closeConn() throws SQLException {
+		if (conn != null) {
+			conn.close();
+			System.out.println("connection closed");
+		}
+		
+	}
+	
+}
