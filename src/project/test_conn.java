a3d60e26dbd8354c11adfdb5b142bdeb53504e78
Rich627 <103445968+Rich627@users.noreply.github.com>
2022-12-17 01:39:22 +0800

    add conn, query

--- src/project/test_conn.java
+++ src/project/test_conn.java
@@ -2,94 +2,20 @@ package project;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Scanner;
 
 public class test_conn {
 	
-	private Connection conn;
-	
-	public void createConn() throws ClassNotFoundException, SQLException{
-		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		
-		String urlstr = "jdbc:sqlserver://140.136.158.12:1433;databaseName=Project;user=project_account;password=helloworld";
-		conn = DriverManager.getConnection(urlstr);
-		boolean status = !conn.isClosed();
-		System.out.println("connection status" + status);
-	}
-	
-	public void closeConn() throws SQLException {
-		if (conn != null) {
-			conn.close();
-			System.out.println("connection closed");
-		}
-		
-	}
-	public boolean checkLogin(String user, String pwd)  throws SQLException {
-		Statement state = conn.createStatement();
-		String sqlstr = "select * from Account where username = '" + user + "' and userpwd = '" + pwd + "'";
-		System.out.println("sqlstr : " + sqlstr);
-		
-		ResultSet rs = state.executeQuery(sqlstr);
-		boolean status = rs.next();
-		rs.close();
-		
-		if(status) {
-			return true;
-		}
-		return false;
-	}
+	 public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		  Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		//System.out.println("Please Input Your Username or Password : ");
-		String user = "", pwd = "";
-		
-		Scanner s1 = new Scanner(System.in);
-		
-		System.out.println("Username : ");
-		user = s1.next();
-		
-		System.out.println("Password : ");
-		pwd = s1.next();
-		
-		System.out.println("user : " + user + " pwd : " + pwd);
-		s1.close();
-		
-		System.out.println("user_len : " + user.length() + " dustypwd_len : " + pwd.length());
-		
-		if(user.length() != 0 && pwd.length() != 0) {
-			test_conn check = new test_conn();
-			check.createConn();
-			
-			boolean result = check.checkLogin(user, pwd);
-			System.out.println("result : " + result);
-			
-			check.closeConn();
-		}
-	}
-	
-	public void action1(String[] args) throws SQLException, ClassNotFoundException {
-		if(args.length == 2) {
-			String user = args[0];
-			String pwd = args[1];
-			System.out.println("user : " + user + " pwd : " + pwd);
-			
-			test_conn check = new test_conn();
-			check.createConn();
-			
-			boolean result = check.checkLogin(user, pwd);
-			System.out.println("result : " + result);
-			
-			check.closeConn();
-		}else {
-			System.out.println("Please input correct username or password.");
-		}
+		  String urlstr = "jdbc:sqlserver://140.136.158.112:1433;encrypt=true;trustServerCertificate=true;databaseName=Project;user=project_account;password=helloworld";
+		  Connection conn = DriverManager.getConnection(urlstr);
 
-	}
-
-	
+		
+		  boolean status = !conn.isClosed();
+		  System.out.println("connect status:" + status);
+		  
+		  conn.close();
+		 }
 }
-	
-
---
+++ src/project/test_login.java
@@ -0,0 +1,33 @@
+package project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class test_login {
+	 public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		  Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+		  String urlstr = "jdbc:sqlserver://140.136.158.112:1433;;encrypt=true;trustServerCertificate=true;databaseName=Project;user=project_account;password=helloworld";
+		  Connection conn = DriverManager.getConnection(urlstr);
+
+		  
+		  boolean status = !conn.isClosed();
+		  System.out.println("connect status:" + status);
+
+		  //Query action  
+		  Statement state = conn.createStatement();
+		  String sqlstr = "select * from carid ";
+		  ResultSet rs = state.executeQuery(sqlstr);
+		  
+		  
+		  while(rs.next()) {
+			  System.out.println(rs.getString(1) + "  " +rs.getInt(2));
+		   }  
+		  rs.close();
+		  state.close();
+		  conn.close();
+		 }
+}
