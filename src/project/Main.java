b857f9d135a4b76254b6c4fa0388e4b198300889
Rich627 <103445968+Rich627@users.noreply.github.com>
2022-12-19 21:07:38 +0800

    add login

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,5 +1,13 @@
 package project;
 
 public class Main {
-
+	//老師要登入
+	//login
+	//輸入車牌
+	//還沒寫
+	//產生停多久
+	//寫一半
+	//輸出費率
+	//還沒寫
 }
+
--- src/project/test_login.java
+++ src/project/test_login.java
@@ -14,17 +14,14 @@ public class test_login {
 	public void createConn() throws ClassNotFoundException, SQLException {
 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
-		 String urlstr = "jdbc:sqlserver://140.136.158.112:1433;encrypt=true;trustServerCertificate=true;databaseName=Project;user=project_account;password=helloworld";
+		String urlstr = "jdbc:sqlserver://140.136.158.112:1433;encrypt=true;trustServerCertificate=true;databaseName=Project;user=project_account;password=helloworld";
 		conn = DriverManager.getConnection(urlstr);
-		boolean status = !conn.isClosed();
-		System.out.println("connection status : " + status);
 	}
 	
 	public boolean checkLogin(String user, String pwd)  throws SQLException {
 		Statement state = conn.createStatement();
-		String sqlstr = "select * from userid where username = '" + user + "' and password = '" + pwd + "'";
-		System.out.println("sqlstr : " + sqlstr);
-		
+		String sqlstr = "select * from data = " ;
+	
 		ResultSet rs = state.executeQuery(sqlstr);
 		boolean status = rs.next();
 		rs.close();
@@ -36,56 +33,31 @@ public class test_login {
 	}
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		//System.out.println("Please Input Your Username or Password : ");
-		String user = "", pwd = "";
-		
-		Scanner s1 = new Scanner(System.in);
-		
-		System.out.println("Username : ");
-		user = s1.next();
-		
-		System.out.println("Password : ");
-		pwd = s1.next();
-		
-		System.out.println("user : " + user + " pwd : " + pwd);
-		s1.close();
-		
-		System.out.println("user_len : " + user.length() + " dustypwd_len : " + pwd.length());
-		
-		if(user.length() != 0 && pwd.length() != 0) {
-			test_login check = new test_login();
-			check.createConn();
-			
-			boolean result = check.checkLogin(user, pwd);
-			System.out.println("result : " + result);
-			
-			check.closeConn();
+		while (true){
+			String user = "", pwd = "";
+			Scanner s1 = new Scanner(System.in);
+			System.out.println("Username : ");
+			user = s1.next();	
+			System.out.println("Password : ");
+			pwd = s1.next();
+			if(user.equals("teacher_account") &&  pwd.equals("password")) {
+				test_login check = new test_login();
+				check.createConn();
+				check.closeConn();
+				System.out.println("succ");
+				break;
+				}
+			else {
+				System.out.println("wrong");
+			}
 		}
 	}
+			
 	
-	public void action1(String[] args) throws SQLException, ClassNotFoundException {
-		if(args.length == 2) {
-			String user = args[0];
-			String pwd = args[1];
-			System.out.println("user : " + user + " pwd : " + pwd);
-			
-			test_login check = new test_login();
-			check.createConn();
-			
-			boolean result = check.checkLogin(user, pwd);
-			System.out.println("result : " + result);
-			
-			check.closeConn();
-		}else {
-			System.out.println("Please input correct username or password.");
-		}
-
-	}
 	
 	public void closeConn() throws SQLException {
 		if (conn != null) {
 			conn.close();
-			System.out.println("connection closed");
 		}
 	}
 
